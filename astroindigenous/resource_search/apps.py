6dba2e1a4a1e0e78774b3af925b3f191516f3295
Heidi Anne White <white@astro.utoronto.ca>
2022-01-15 15:06:34 -0800

    Create initial resource model

---
+++ astroindigenous/resource_search/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ResourceSearchConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'resource_search'
---
+++ astroindigenous/resource_search/models.py
@@ -0,0 +1,31 @@
+from datetime import date
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.translation import gettext_lazy as _
+
+
+INDIGENOUS_AUTHOR_CHOICES = (
+    (None, "Unknown"),
+    (False, "No"),
+    (True, "Yes")
+)
+
+def less_than_current_year(value):
+    if value > date.today().year:
+        raise ValidationError(
+            _('%(requested_year) is in the future!'),
+            params={'requested_year': value}
+        )
+
+# Create your models here.
+class Resource(models.Model):
+    author = models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
+    links = models.TextField()
+    rec_type = models.CharField(max_length=128)
+    summary = models.TextField(blank = True)
+    indigenous_author = models.BooleanField(choices = INDIGENOUS_AUTHOR_CHOICES, null = True) 
+    year = models.IntegerField(blank=True, validators=[
+        less_than_current_year, MinValueValidator(1970), MaxValueValidator(9999)
+    ])
