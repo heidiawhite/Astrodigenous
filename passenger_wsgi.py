63541c28ca789a2d7d2fd858c4e67f7f18fbadf5
Heidi Anne White <white@astro.utoronto.ca>
2022-01-15 14:06:22 -0800

    Migrate to django

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,17 @@
 import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'heidiwhite.net', 'venv', 'bin', 'python3')
-if sys.executable != INTERP:
-        os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
+INTERP = "/home/dh_bv2ah4/heidiwhite.net/venv/bin/python3"
 
-os.environ['FLASK_DEBUG'] = 'true'
+#INTERP is present twice so that the new python interpreter 
+#knows the actual executable path 
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
-sys.path.append('astrodigineous')
-from astrodigineous import app as application
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/astroindigenous')  #You must add your project here
+
+sys.path.insert(0,cwd+'/venv/bin')
+sys.path.insert(0,cwd+'/venv/lib/python3.8/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "astroindigenous.settings"
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
