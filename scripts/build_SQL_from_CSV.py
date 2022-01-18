d2e5824bd3a7a3f45625b53261d8db81002fb1d7
Heidi Anne White <white@astro.utoronto.ca>
2022-01-17 19:00:05 -0800

    Update scripts to import initial records

--- scripts/build_SQL_from_CSV.py
+++ scripts/build_SQL_from_CSV.py
@@ -4,11 +4,13 @@ import pandas as pd
 import mysql.connector
 from mysql.connector import errorcode
 
+from sqlalchemy import types, create_engine
+
 #find CSV files in the working directory
 csv_files = []
 for file in os.listdir(os.getcwd()):
-	if file.endswith('.csv')
-		csv.files.append(file)
+	if file.endswith('.csv'):
+		csv_files.append(file)
 
 #create a new directory for the datasets
 try:
@@ -24,18 +26,20 @@ for csv in csv_files:
 	os.system(mv_file)
 
 file_path = os.getcwd()+'/'+dataset_dir+'/'
-	for file in csv_files:
-		try:
-			df[file] = pd.read_csv(file_path+file)
-		except UnicodeDecodeError:
-			df[file] = pd.read_csv(file_path+file, encoding='ISO-8859-1')
+
+df = {}
+for file in csv_files:
+	try:
+		df[file] = pd.read_csv(file_path+file)
+	except UnicodeDecodeError:
+		df[file] = pd.read_csv(file_path+file, encoding='ISO-8859-1')
 
 #clean up the table file names and column headers to remove any capital letters, symbols, or unhelpful characters
 
 for i in csv_files:
 	dataframe = df[i]
 
-	clean_tbl_name = file.lower().replace(" ","_").replace("?","").replace("-","_")\
+	clean_tbl_name = i.lower().replace(" ","_").replace("?","").replace("-","_")\
 	.replace(r"/","_").replace("\\","_").replace(")","").replace(r"(","").replace("%","").replace("$","")
 
 	#strip the file extension from clean table name
@@ -48,11 +52,12 @@ for i in csv_files:
 	#create a dictionary to convert between pandas datatypes and SQL datatypes
 
 	dtype_list = {
-		'object': 'varchar'
-		'float64': 'float'
-		'int64': "int"
-		'datetime64': 'timestamp'
-		'timedelta64[ns]': 'varchar'
+		'object': 'varchar',
+		'float64': 'float',
+		'int64': 'int',
+		'bool': 'bool',
+		'datetime64': 'timestamp',
+		'timedelta64[ns]': 'varchar',
 	}
 
 	#now map out a scheme for the columns in the table
@@ -63,9 +68,9 @@ for i in csv_files:
 
 	#connect to the database
 	host = 'mysql.heidiwhite.net'
-	dbname = 'astrodigenous'
+	dbname = 'astroindigenous'
 	user = 'hwhite'
-	password = 'XXX'
+	password = 'lilith31'
 
 	config = {
 	  'user': user,
@@ -86,3 +91,24 @@ for i in csv_files:
 	    print(err)
 	else:
 	  cnx.close()
+
+	 mycursor = mydb.cursor()
+
+
+dtype_list = {
+	'resource_id': types.VARCHAR(length=146),
+	'nation_tradition': types.VARCHAR(length=146),
+	'settler_territory': types.VARCHAR(length=146),
+	'author': types.VARCHAR(length=146),
+	'title': types.VARCHAR(length=146),
+	'link': types.VARCHAR(length=146),
+	'rec_type': types.VARCHAR(length=146),
+	'rec_summary': types.VARCHAR(length=146),
+	'publ_lang': types.VARCHAR(length=146),
+	'indigenous_author': types.VARCHAR(length=16),
+	'year': types.VARCHAR(length=16)
+}
+
+df.to_sql(name="ResourceTable", con=engine, if_exists="replace", index=False, dtype=dtype_list)
+
+engine = create_engine("mysql+pymysql://" + user + ":" + password + "@" + host + "/" + database)
